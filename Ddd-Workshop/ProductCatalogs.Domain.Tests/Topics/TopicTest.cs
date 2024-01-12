f2b674a1cac7db57555150073f4cf0785bd249fa
Mojtaba Hasanlou <hasanlou.mojtaba@gmail.com>
2024-01-12 12:01:33 +0330

    Add Topic

---
+++ Ddd-Workshop/ProductCatalogs.Domain.Tests/Topics/TopicTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using ProductCatalogs.Domain.Topics;
+using Xunit;
+
+namespace ProductCatalogs.Domain.Tests.Topics
+{
+    public class TopicTest
+    {
+        [Fact]
+        public void Define_a_Topic()
+        {
+            var title = "Math";
+            var topic = CreateTopic(title);
+
+            topic.Should().NotBeNull();
+            topic.Title.Should().Be(title);
+        }
+
+        private static Topic CreateTopic(string title)
+        {
+            var topic = new Topic(title);
+            return topic;
+        }
+    }
+}
---
+++ Ddd-Workshop/ProductCatalogs.Domain/Topics/Topic.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductCatalogs.Domain.Topics
+{
+    public class Topic
+    {
+        public Topic(string title)
+        {
+            Title = title;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+    }
+}

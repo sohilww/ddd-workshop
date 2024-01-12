a354991bba64199277e5d4b1e9eb8ad8f4684542
Soheil Karami <soheilkarami@outlook.com>
2024-01-12 13:16:41 +0330

    add gurd for SpecificTime

---
+++ Ddd-Workshop/ProductCatalogs.Domain/Sessions/SpecificTime.cs
@@ -0,0 +1,21 @@
+﻿using Ackee.Domain.Model;
+
+namespace ProductCatalogs.Domain.Sessions;
+
+public class SpecificTime : ValueObject
+{
+    public SpecificTime(TimeSpan startTime, TimeSpan endTime)
+    {
+        GurdForDateValidate(startTime, endTime);
+        StartTime = startTime;
+        EndTime = endTime;
+    }
+
+    public TimeSpan StartTime { get;private set; }
+    public TimeSpan EndTime { get;private set; }
+
+    public void GurdForDateValidate(TimeSpan startTime, TimeSpan endTime)
+    {
+        if (endTime <= startTime) throw new Exception("Not Valid");
+    }
+}
